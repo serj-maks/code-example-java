b29c362d5a55533dc2096aa5717d4549516ecbf1
Sergey Maksimov(work pc) <sergeymaksimov1993@gmail.com>
2022-01-31 14:18:42 +0300

    31 01 2022

--- src/main/java/com/vault/package-info.java
+++ src/main/java/com/vault/package-info.java
@@ -1,12 +1,12 @@
 package com.vault;
 
-/**
- * this package is root for my java code examples
- * here packages with categories + "no category" package
- *
- * "java" prefix for packages with keyword name
- * if i want to name "interface" package - write "javainterface"
- *
- * @author Sergey Maksimov
+/*
+  this package is root for my java code examples
+  here packages with categories + "no category" package
+
+  "java" prefix for packages with keyword name
+  if i want to name "interface" package - write "javainterface"
+
+  @author Sergey Maksimov
  * @version 1.1
  */
