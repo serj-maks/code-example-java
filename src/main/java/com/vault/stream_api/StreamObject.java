dc434c89db660be69f31ecda31caa828ae7a68f8
Sergey Maksimov <sergeymaksimov1993@gmail.com>
2022-04-22 17:43:10 +0300

    22 04 22

---
+++ src/main/java/com/vault/stream_api/StreamObject.java
@@ -0,0 +1,25 @@
+package com.vault.stream_api;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamObject {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("serj");
+        list.add("nat");
+        list.add("yaro");
+
+        String findFirstElement = list.stream().findFirst().orElse(null);
+        System.out.println(findFirstElement);
+
+        List<String> stringList = Arrays.asList("serj", "nat", "yaro");
+        stringList.stream().filter("serj"::equals).count();
+
+        Stream.of("serj", "nat", "yaro").skip(stringList.size() - 1).findFirst().orElse(null);
+
+    }
+}
