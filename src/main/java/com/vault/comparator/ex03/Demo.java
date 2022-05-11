be5d18ce7ee43e0ed14626d2dc642e064a4dc4d9
Sergey Maksimov(work pc) <sergeymaksimov1993@gmail.com>
2022-05-11 17:21:10 +0300

    11.05.2022

---
+++ src/main/java/com/vault/comparator/ex03/Demo.java
@@ -0,0 +1,17 @@
+package com.vault.comparator.ex03;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Demo {
+    public static void main(String[] args) {
+        // создание компаратора в конструкторе
+        Map<User, Integer> map = new TreeMap<>(Comparator.comparing(o -> o.getAge()));
+        map.put(new User("serj", 28), 0);
+        map.put(new User("nat", 29), 0);
+        map.put(new User("yaro", 1), 0);
+
+        System.out.println(map);
+    }
+}
--- src/main/java/com/vault/java_collection_framework/list/array_list/code_for_testing/Demo.java
+++ src/main/java/com/vault/java_collection_framework/list/array_list/code_for_testing/Demo.java
@@ -2,13 +2,14 @@ package com.vault.java_collection_framework.list.array_list.code_for_testing;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class Demo {
     public static void main(String[] args) {
         Car volvo = new Car("XS90",130);
         Car lada = new Car("priora",90);
         Car bmw = new Car("X5",150);
-        ArrayList<Car> cars = new ArrayList<>();
+        List<Car> cars = new ArrayList<>();
         Collections.addAll(cars, volvo, lada, bmw);
         Car mazda = new Car("3", 120);
 
